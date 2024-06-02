d9fc498eef3c8618acb79e281619cb3b7f156227
Adeel-CS <135587537+Adeel-CS@users.noreply.github.com>
2024-06-02 20:30:24 +0500

    First Commit for the array code in C#

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public static void Main(string[] args){
+        Console.WriteLine("Just getting started with the array");
+        int[] nums = {1,3,52,3,5,3};
+        int target = 6;
+        int[] result = TwoSum(nums, target);
+        Console.WriteLine($"The indices are: [{result[0]}, {result[1]}]");
+
+    }
+
+public static int[]? TwoSum(int[] nums, int target)
+{
+    var indexWithNumber = new Dictionary<int, int>();
+    var numberByIndex = new Dictionary<int, List<int>>();
+    for (var i = 0; i < nums.Length; ++i)
+    {
+        indexWithNumber.Add(i, nums[i]);
+
+        if (numberByIndex.ContainsKey(nums[i]))
+        {
+            numberByIndex[nums[i]].Add(i);
+        }
+        else
+        {
+            numberByIndex.Add(nums[i], new List<int>() { i });
+        }
+    }
+
+    for (var i = 0; i < nums.Length; ++i)
+    {
+        var remain = target - nums[i];
+
+        if (numberByIndex.TryGetValue(remain, out var indices))
+        {
+            if (indices.Count == 1 && indices.First() == i)
+            {
+                continue;
+            }
+            var index = numberByIndex[remain].Where(x => x != i).First();
+            return new int[] { i, index };
+        }
+    }
+
+    return null;
+}
+
+}
---
+++ Solution.cs
@@ -0,0 +1,54 @@
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+
+// public class Solutions
+// {
+//     public static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello, C#!");
+
+//         // Example usage of the TwoSum method
+//         int[] nums = { 2, 7, 11, 15 };
+//         int target = 9;
+//         int[] result = TwoSum(nums, target);
+//         Console.WriteLine($"Indices: [{result[0]}, {result[1]}]");
+//     }
+    
+
+//     public static int[] TwoSum(int[] nums, int target)
+//     {
+//         var indexWithNumber = new Dictionary<int, int>();
+//         var numberByIndex = new Dictionary<int, List<int>>();
+//         for (var i = 0; i < nums.Length; ++i)
+//         {
+//             indexWithNumber.Add(i, nums[i]);
+
+//             if (numberByIndex.ContainsKey(nums[i]))
+//             {
+//                 numberByIndex[nums[i]].Add(i);
+//             }
+//             else
+//             {
+//                 numberByIndex.Add(nums[i], new List<int>() { i });
+//             }
+//         }
+
+//         for (var i = 0; i < nums.Length; ++i)
+//         {
+//             var remain = target - nums[i];
+
+//             if (numberByIndex.TryGetValue(remain, out var indices))
+//             {
+//                 if (indices.Count == 1 && indices.First() == i)
+//                 {
+//                     continue;
+//                 }
+//                 var index = numberByIndex[remain].Where(x => x != i).First();
+//                 return new int[] { i, index };
+//             }
+//         }
+
+//         return null;
+//     }
+// }
